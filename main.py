24e66f323749d4dfa2bda2b7484ed13286904c75
정준호 <129667716+juhhoho@users.noreply.github.com>
2024-10-07 01:52:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from sparc_simulator import SPARCSimulator
 
 if __name__ == "__main__":
     simulator = SPARCSimulator()
-    simulator.load_program_from_file("asm/test.s")
+    simulator.load_program_from_file("")
     simulator.execute("main")
